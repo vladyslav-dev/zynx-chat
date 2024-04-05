78026fa153f2079eefbfc073f29cb87bc7fc63ce
Vladyslav Hryhorenko <grigovlad09112002@gmail.com>
2024-04-06 01:29:51 +0300

    server websocket chat implementation

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"server/db"
 	"server/internal/user"
+	"server/internal/ws"
 	"server/router"
 )
 
@@ -20,6 +21,11 @@ func main() {
 	userSvc := user.NewService(userRep)
 	userHandler := user.NewHandler(userSvc)
 
-	router.InitRouter(userHandler)
+	hub := ws.NewHub()
+	wsHandler := ws.NewHandler(hub)
+
+	go hub.Run()
+
+	router.InitRouter(userHandler, wsHandler)
 	router.Start("0.0.0.0:8080")
 }
--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/gin-gonic/gin v1.9.1
+	github.com/gorilla/websocket v1.5.1
 	github.com/lib/pq v1.10.9
 	golang.org/x/crypto v0.21.0
 )
