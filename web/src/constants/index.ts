be0e72ea9dce964b833686d0e5116aecaf500c02
vladyslav_developer <vhryhorenko@gbsfo.com>
2024-08-12 23:28:20 +0300

    env file for web app

--- web/src/constants/index.ts
+++ web/src/constants/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
-export const API_URL = `${BASE_URL}/api`;
-export const WEBSOCKET_URL = 'ws://localhost:8080';
+export const BASE_URL = process.env.SERVER_URL || 'https://whisper-warp-production.up.railway.app';
+export const API_URL = process.env.API_URL || `${BASE_URL}/api`;
+export const WEBSOCKET_URL = process.env.WEBSOCKET_URL || 'ws://whisper-warp-production.up.railway.app';
