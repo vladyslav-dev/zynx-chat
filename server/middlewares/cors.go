5319f9946b2ec7f6f13eff3dfee1f51a49775464
vladyslav_developer <vhryhorenko@gbsfo.com>
2024-08-12 23:49:32 +0300

    server env

--- server/middlewares/cors.go
+++ server/middlewares/cors.go
@@ -1,10 +1,16 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var CLIENT_URL = os.Getenv("CLIENT_URL")
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", CLIENT_URL)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
