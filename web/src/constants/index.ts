ca2189fd7b295444af40346e4660593a6447498b
vladyslav_developer <vhryhorenko@gbsfo.com>
2024-08-17 00:19:02 +0300

    Changed ws to wss

--- web/src/constants/index.ts
+++ web/src/constants/index.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.SERVER_URL || 'https://whisper-warp-production.up.railway.app';
 export const API_URL = process.env.API_URL || `${BASE_URL}/api`;
-export const WEBSOCKET_URL = process.env.WEBSOCKET_URL || 'ws://whisper-warp-production.up.railway.app';
+export const WEBSOCKET_URL = process.env.WEBSOCKET_URL || 'wss://whisper-warp-production.up.railway.app';
